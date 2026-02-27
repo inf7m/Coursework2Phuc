ff2ea3b9204e4ff760b11f7312484976b9ded0fd
Sebastian Dang <85420923+inf7m@users.noreply.github.com>
2026-02-27 12:19:35 +0800

    Create config.py
    Push

---
+++ config.py
@@ -0,0 +1,11 @@
+# This file is about the configuration for the program
+# There are three thing you can change in here
+
+# First, Data Folder is a specify location output for generateDatasets.py
+DATA_FOLDER = "data"
+
+# Second, Keyword/phrase to search for both sequential and parallel program
+KEYWORD = "error"
+
+# Third, Number of threads/processes for parallel version
+NUM_WORKERS = 4
